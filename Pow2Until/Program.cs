815589117fbbb945c946044c54b56a8172c0b1ac
Miguel Filipe <miguelfilipe.dev@gmail.com>
2025-03-05 22:31:10 +0000

    Added Pow2Until

---
+++ Pow2Until/Program.cs
@@ -0,0 +1,29 @@
+﻿﻿using System ;
+
+
+namespace ChangeString
+{
+      
+    class Program
+    {
+           
+        static void Main (string[] args)
+        {
+            
+            PowersOf2Until5();
+            PowersOf2Until5();
+                    
+        }
+        private static void PowersOf2Until5()
+        {
+            for (int i = 1; i <= (1 << 5); i = i << 1)
+                {
+                    Console.WriteLine(i);
+                }
+        }
+               
+
+
+            
+    }
+}
