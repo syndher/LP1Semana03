ada0cf2341c3e47f39f1817248c1d9a5f37bd6da
Miguel Filipe <miguelfilipe.dev@gmail.com>
2025-03-05 19:29:36 +0000

    Added Requests

---
+++ ChangeString/Program.cs
@@ -0,0 +1,38 @@
+﻿﻿using System ;
+using System.ComponentModel;
+using System.Formats.Asn1;
+using System.Runtime.CompilerServices;
+
+
+namespace RightSize
+{
+      
+    class Program
+    {
+           
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Write a line");
+            string resposta = Console.ReadLine();
+
+            Console.WriteLine("Write a character");
+            string carater = Console.ReadLine();
+
+            string substituto = "x";
+
+            foreach (char caraterin in resposta);
+
+
+                
+                        
+            
+
+                    
+        }
+
+               
+
+
+            
+    }
+}
